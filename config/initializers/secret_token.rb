38ded6d27b8d2161b8f78efb105a053d3dcd9c86
Mel Zhang <zeemelon@gmail.com>
2014-02-18 15:44:53 -0800

    created melzhang and changed the db from sqlite3 to postgres by adding the gem 'pg' to the gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Melzhang::Application.config.secret_key_base = '48503a9f1c01a5c4f43164e00cf535f0253c32e6afdbc00c5feaab7f4bd41ea0de5f5fc4965168d74bcfd11c246fae1ccbff7450a6e0e95ed7e9e53c713e666b'
