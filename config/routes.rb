e2e1b7f394a1e271ced8758788de9cbbc13d38d8
Mel Zhang <zeemelon@gmail.com>
2014-03-08 09:06:51 -0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Melzhang::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: 'main#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
