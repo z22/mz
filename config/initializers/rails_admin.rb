a94b78eec87527db334d24bef942e07fa481d371
Mel Zhang <zeemelon@gmail.com>
2014-10-14 18:18:29 -0700

    configured rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -31,4 +31,16 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+
+
+RailsAdmin.config do |config|
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'm' && password == 'z'
+    end
+  end
+
+  config.main_app_name { ['mz', 'Admin'] }
+  end
+
 end
