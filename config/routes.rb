e7edcbfa7fdb0571e55fce1806a512dfd0bfe010
Mel Zhang <zeemelon@gmail.com>
2014-10-14 17:54:22 -0700

    edited About Me page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Melzhang::Application.routes.draw do
   resources :contacts, only: [:new, :create]
 
 
+  match ':controller(/:action(/:id(.:format)))', :via => :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
